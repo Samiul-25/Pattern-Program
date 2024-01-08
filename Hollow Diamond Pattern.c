c13cc5bef697adcb551888d569abef727c5c99eb
Md.Samiul Alim <53310538+Samiul-25@users.noreply.github.com>
2024-01-09 01:00:19 +0600

    Add files via upload

---
+++ Hollow Diamond Pattern.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    // first outer loop to iterate through each row
+    for (int i = 0; i < 2 * n - 1; i++) {
+        // assigning values to the comparator according to the row number
+        int comp;
+        if (i < n) {
+            comp = n - i - 1;
+        } else {
+            comp = i - n + 1;
+        }
+
+        // first inner loop to print leading whitespaces
+        for (int j = 0; j < comp; j++) {
+            printf(" ");
+        }
+
+        // second inner loop to print star * and inner whitespaces
+        for (int k = 0; k < 2 * (n - comp) - 1; k++) {
+            if (k == 0 || k == 2 * (n - comp) - 2) {
+                printf("* ");
+            } else {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
