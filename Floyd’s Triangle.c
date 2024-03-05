705154f138e87c580c084837124f3fb26f338eb9
Md.Samiul Alim <53310538+Samiul-25@users.noreply.github.com>
2024-03-05 23:57:20 +0600

    Update Floyd’s Triangle.c

--- Floyd’s Triangle.c
+++ Floyd’s Triangle.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int rows, count = 1;
+    int rows,i,j,count = 1;
 
     printf("Enter the number of rows for Floyd's Triangle: ");
     scanf("%d", &rows);
 
     printf("Floyd's Triangle with %d rows:\n", rows);
-    for (int i = 1; i <= rows; i++) {
-        for (int j = 1; j <= i; j++) {
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= i; j++) {
             printf("%d ", count);
             count++;
         }
