f44d6c0a83f4f905db4bf303d103e3866233b444
Md.Samiul Alim <53310538+Samiul-25@users.noreply.github.com>
2024-03-05 23:56:39 +0600

    Update Diamond.c

--- Diamond.c
+++ Diamond.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int n,i,j,k,comp;
 
     printf("Enter number of rows: ");
     scanf("%d", &n);
 
     // first outer loop to iterate through each row
-    for (int i = 0; i < 2 * n - 1; i++) {
+    for (i = 0; i < 2 * n - 1; i++) {
 
-        // assigning values to the comparator according to 
-        // the row number
-        int comp;
+        // assigning values to the comparator according to the row number
         if (i < n) {
             comp = 2 * (n - i) - 1;
         } else {
@@ -19,12 +17,12 @@ int main() {
         }
 
         // first inner loop to print leading whitespaces
-        for (int j = 0; j < comp; j++) {
+        for (j = 0; j < comp; j++) {
             printf(" ");
         }
 
         // second inner loop to print stars *
-        for (int k = 0; k < 2 * n - comp; k++) {
+        for (k = 0; k < 2 * n - comp; k++) {
             printf("* ");
         }
         printf("\n");
